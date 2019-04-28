e44429e116e714e21113da5fa0f4d8e35263084a
jafferveronezi <jafferveronezi@gmail.com>
2019-04-28 14:57:28 -0300

    adiciona mapa na tela de sobre

--- app/src/main/java/br/com/veronezitecnologia/pingatech/view/fragment/AboutFragment.kt
+++ app/src/main/java/br/com/veronezitecnologia/pingatech/view/fragment/AboutFragment.kt
@@ -1,14 +1,27 @@
 package br.com.veronezitecnologia.pingatech.view.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import br.com.veronezitecnologia.pingatech.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_about.*
 
-class AboutFragment : Fragment(){
+
+
+
+
+class AboutFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +30,26 @@ class AboutFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         var rootView = inflater!!.inflate(R.layout.fragment_about, container, false)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+
         return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val zoom = 17
+
+        // Add a marker in Sydney and move the camera
+        val pingaTechLocation = LatLng(-21.1072852,-45.8302261)
+        mMap.addMarker(MarkerOptions().position(pingaTechLocation).title(context?.getString(R.string.map_location)))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pingaTechLocation))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pingaTechLocation, zoom.toFloat()));
     }
 
 }
